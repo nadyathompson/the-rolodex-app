df1854ec141e03937585858aa89d788383533ff2
nadyathompson <127708727+nadyathompson@users.noreply.github.com>
2023-03-23 15:40:19 -0400

    Created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World')
-let favoriteFood = 'Instant Ramen'
-document.write(favoriteFood)
+let pokemonList = [{name: "Bulbasaur", height: 7, type: ['grass','poison']}, 
+                   {name: "Charmander", height: 6,type: ['fire']}, 
+                   {name: "Squirtle", height: 5,type: ['water']},
+                   {name: "Pikachu", height: 4,type: ['electric']}];
+console.log(pokemonList)
