724ac53c9e1520da468f1a75ad8179211c3c4afb
nadyathompson <127708727+nadyathompson@users.noreply.github.com>
2023-03-24 14:36:03 -0400

    created a for loop to display the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-let pokemonList = [{name: "Bulbasaur", height: 7, type: ['grass','poison']}, 
-                   {name: "Charmander", height: 6,type: ['fire']}, 
-                   {name: "Squirtle", height: 5,type: ['water']},
-                   {name: "Pikachu", height: 4,type: ['electric']}];
-console.log(pokemonList)
+let pokemonList = [{name: "Bulbasaur", height: 0.7, type: ['grass','poison']}, 
+                   {name: "Charmander", height: 0.6, type: ['fire']}, 
+                   {name: "Squirtle", height: 1.9, type: ['water']},
+                   {name: "Pikachu", height: 0.4, type: ['electric']}];
+
+for (i=0; i<pokemonList.length; i++){
+    let pokemonInfo = pokemonList[i].name + ' (height: ' + pokemonList[i].height + ', ' + pokemonList[i].type + ' type)';
+    if (pokemonList[i].height > 1.8) {
+        pokemonInfo += ' - wow that is big';
+        }
+    pokemonInfo += '<br><br>';
+    document.write(pokemonInfo);
+}
+
